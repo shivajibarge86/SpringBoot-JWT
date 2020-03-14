c6fcde8eb2c638f35c89d4c04476f183817d1dd7
shivajibarge86 <shivajibarge86@gmail.com>
2020-03-14 18:57:47 +0530

    first gitbash commit

--- src/main/java/com/inventory/productinventory/DemoApplication.java
+++ src/main/java/com/inventory/productinventory/DemoApplication.java
@@ -3,6 +3,10 @@ package com.inventory.productinventory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author shivaji barge
+ *
+ */
 @SpringBootApplication
 public class DemoApplication {
 
